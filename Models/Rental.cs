e1ed89299d4faccc786f998f18830ad5c45a561c
Michał Żukowski <s26133@pjwstk.edu.pl>
2026-03-30 23:07:39 +0200

    dodanie modelu wypozyczenia

---
+++ Models/Rental.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAPBD01.Models
+{
+    public class Rental
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public Guid EquipmentId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public decimal PenaltyFee { get; set; }
+
+        public bool IsOverdue => !ReturnDate.HasValue && DateTime.Now > DueDate;
+    }
+}
