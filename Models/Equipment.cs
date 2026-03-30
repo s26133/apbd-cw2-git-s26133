fb463ff02acda79f74be5662d51e4c080e4ad678
Michał Żukowski <s26133@pjwstk.edu.pl>
2026-03-30 22:49:49 +0200

    Dodanie modeli dla sprzętu i użytkownika

---
+++ Models/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAPBD01.Models
+{
+    public abstract class Equipment
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public EquipmentStatus Status { get; set; } = EquipmentStatus.Available;
+
+        protected Equipment(string name) { Name = name; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAPBD01.Models
+{
+    public abstract class User
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract int MaxActiveRentals { get; }
+
+        protected User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
