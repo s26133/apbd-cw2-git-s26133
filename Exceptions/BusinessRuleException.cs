d770eae485a3dc196f3a9ef05be7c23c029ec6da
Michał Żukowski <s26133@pjwstk.edu.pl>
2026-03-30 23:22:18 +0200

    mehcanizm kar i wyjatki

---
+++ Exceptions/BusinessRuleException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAPBD01.Exceptions
+{
+    public class BusinessRuleException : Exception
+    {
+        public BusinessRuleException(string message) : base(message) { }
+    }
+}
---
+++ Services/PenaltyCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAPBD01.Services
+{
+    public interface IPenaltyCalculator
+    {
+        decimal CalculatePenalty(DateTime dueDate, DateTime returnDate);
+    }
+
+    public class StandardPenaltyCalculator : IPenaltyCalculator
+    {
+        public decimal CalculatePenalty(DateTime dueDate, DateTime returnDate)
+        {
+            if (returnDate <= dueDate) return 0m;
+            return (returnDate - dueDate).Days * 10m;
+        }
+    }
+}
