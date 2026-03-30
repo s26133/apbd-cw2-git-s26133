807e91bc5a1e82e1a14252d9fd5176f7f3555d4e
Michał Żukowski <s26133@pjwstk.edu.pl>
2026-03-30 23:03:51 +0200

    dodanie typow sprzetu i ról uzytkownikow

---
+++ Models/SpecificEquipment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAPBD01.Models
+{
+    public class Laptop : Equipment
+    {
+        public int RamGigabytes { get; set; }
+        public string Processor { get; set; }
+        public Laptop(string name, int ram, string processor) : base(name) { RamGigabytes = ram; Processor = processor; }
+    }
+
+    public class Projector : Equipment
+    {
+        public int ResolutionX { get; set; }
+        public int Lumens { get; set; }
+        public Projector(string name, int resX, int lumens) : base(name) { ResolutionX = resX; Lumens = lumens; }
+    }
+
+    public class Camera : Equipment
+    {
+        public int Megapixels { get; set; }
+        public string MountType { get; set; }
+        public Camera(string name, int megapixels, string mount) : base(name) { Megapixels = megapixels; MountType = mount; }
+    }
+}
---
+++ Models/SpecificUsers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramAPBD01.Models
+{
+    public class Student : User
+    {
+        public override int MaxActiveRentals => 2;
+        public Student(string firstName, string lastName) : base(firstName, lastName) { }
+    }
+
+    public class Employee : User
+    {
+        public override int MaxActiveRentals => 5;
+        public Employee(string firstName, string lastName) : base(firstName, lastName) { }
+    }
+}
